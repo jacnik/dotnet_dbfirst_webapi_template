65e7e327ca7728dd40092e40cea69e6a12092c00
Jacnik <you@example.com>
2017-10-07 10:39:20 +0000

    Added first methods to ContosoRepository

--- ApiTemplate.ContosoUniversity.DAL/ContosoRepository.cs
+++ ApiTemplate.ContosoUniversity.DAL/ContosoRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ApiTemplate.ContosoUniversity.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ApiTemplate.ContosoUniversity.DAL
 {
@@ -8,5 +12,28 @@ namespace ApiTemplate.ContosoUniversity.DAL
          * Models folder was generated using the following command:
          * dotnet ef dbcontext scaffold "Server=(localdb)\mssqllocaldb;Database=ContosoUniversity1;Trusted_Connection=True;MultipleActiveResultSets=true" -o Models Microsoft.EntityFrameworkCore.SqlServer -c "SchoolContext" -f
          */
+
+        private readonly SchoolContext context;
+
+        public ContosoRepository(SchoolContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Student>> GetStudents()
+        {
+            return await this.context.Student.ToListAsync();
+        }
+
+        public async Task<Student> Details(int id)
+        {
+            var student = await this.context.Student
+                .Include(s => s.Enrollment)
+                    .ThenInclude(e => e.Course)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.Id == id);
+
+            return student;
+        }
     }
 }
