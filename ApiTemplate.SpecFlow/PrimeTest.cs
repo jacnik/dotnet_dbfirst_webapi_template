cae6c63ffc0724980351ae44072dd1e65bd6624b
Jacnik <you@example.com>
2017-10-08 18:57:18 +0000

    Added fake.csproj for specflow

--- ApiTemplate.SpecFlow/PrimeTest.cs
+++ ApiTemplate.SpecFlow/PrimeTest.cs
@@ -2,6 +2,11 @@ namespace ApiTemplate.SpecFlow
 {
     using Xunit;
 
+    // specFlow for dontnet core: https://github.com/stajs/SpecFlow.NetCore
+    // need to install https://www.microsoft.com/en-us/download/details.aspx?id=40760
+    // This command generated specflow .feature.cs file
+    // C:\Users\jacnik\.nuget\packages\SpecFlow\2.2.1\tools\specflow.exe generateall "C:\Users\jacnik\source\repos\ApiTemplate\ApiTemplate.SpecFlow\ApiTemplate.SpecFlow.csproj.fake.csproj" /force /verbose
+
     public class PrimeService_IsPrimeShould
     {
         public PrimeService_IsPrimeShould()
