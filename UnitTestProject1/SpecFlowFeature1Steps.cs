af129ebba8f22d6e96454c66a5beb221625d9ef8
Jacnik <you@example.com>
2017-10-08 19:57:38 +0000

    .net framework unitTestProject can deal witl SpecFlow

---
+++ UnitTestProject1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Assert.True(1 == 1);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Assert.True(1 == 1);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
